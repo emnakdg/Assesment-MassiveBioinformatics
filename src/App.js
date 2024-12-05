351dc20c32e528de6d9e9ac6c8f9eed651ff20e7
Emin Akdag <emin.akdag20@gmail.com>
2024-12-05 20:10:16 +0300

    App.js güncellendi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // HashRouter'ı kullan
 import CharacterList from './components/CharacterList';
 import CharacterDetail from './components/CharacterDetail';
 
